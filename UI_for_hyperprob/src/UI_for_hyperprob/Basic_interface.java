776bac3324a5fd1ca9c107607a3c35a64103de9d
Oyendrila Dobe <oyendrila.dobe@gmail.com>
2021-06-01 12:49:46 -0400

    Added class structure

---
+++ UI_for_hyperprob/src/UI_for_hyperprob/Basic_interface.java
@@ -0,0 +1,10 @@
+package UI_for_hyperprob;
+import javax.swing.*;
+
+public class Basic_interface {
+
+    //make a function here to create your button and open file
+
+}
+
+
---
+++ UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
@@ -0,0 +1,9 @@
+package UI_for_hyperprob;
+
+public class MainClass {
+
+    public static void main(String args[]) {
+
+        // program execution will start from here. We will create an object of the Basic_interface class here and call its functions
+    }
+}

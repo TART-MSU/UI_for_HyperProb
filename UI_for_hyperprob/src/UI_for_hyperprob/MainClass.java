78927f952c74f6345e7ac63f7742794a2e8ed79e
Oyendrila Dobe <oyendrila.dobe@gmail.com>
2021-06-04 12:51:57 -0400

    Changed class structure

--- UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
+++ UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
@@ -1,17 +1,16 @@
 package UI_for_hyperprob;
 
-import javax.swing.*;
 
+import javax.swing.*;
 public class MainClass {
 
-    public static void main(String args[]) {
-            GUIForm button = new GUIForm();
-            JFrame frame = new JFrame("Your window frame");
-            frame.setContentPane(new button().getJPanel());
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setVisible(true);
-
-        // program execution will start from here. We will create an object of the Basic_interface class here and call its functions
+    public static void main(String[] args) {
+//        new_button form = new new_button();
+//        JFrame frame = new JFrame("Your window name");
+//        frame.setContentPane(new new_button().getJPanel());
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+        editor e = new editor();
     }
 }

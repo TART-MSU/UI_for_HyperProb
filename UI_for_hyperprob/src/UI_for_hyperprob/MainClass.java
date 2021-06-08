1a2cd1ec9ff4c699a376ebedac63dab6e966033d
tarayounessi1 <tarayounessi@gmail.com>
2021-06-08 11:48:46 -0400

    h

--- UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
+++ UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
@@ -1,10 +1,13 @@
 package UI_for_hyperprob;
 
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import javax.swing.*;
+import java.io.*;
 public class MainClass {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 //        new_button form = new new_button();
 //        JFrame frame = new JFrame("Your window name");
 //        frame.setContentPane(new new_button().getJPanel());
@@ -12,5 +15,17 @@ public class MainClass {
 //        frame.pack();
 //        frame.setVisible(true);
         editor e = new editor();
+        String path = "/Users/tarayounessi/Downloads/nqueens.py";
+        ProcessBuilder pb = new ProcessBuilder("python",path).inheritIO();
+        Process p = pb.start();
+        p.waitFor();
+        BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line = "";
+        while ((line = bfr.readLine()) != null) {
+            System.out.println(line);
+        }
     }
+
+
 }
+

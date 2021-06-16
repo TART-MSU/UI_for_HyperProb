72bb1d83afaaf82185f7d57a8a22b605cd73cf47
tarayounessi1 <tarayounessi@gmail.com>
2021-06-16 13:01:53 -0400

    added split screen -contains two panels

--- UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
+++ UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
@@ -11,31 +11,48 @@ import java.io.*;
 public class MainClass {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-//        new_button form = new new_button();
-        // JFrame frame = new JFrame("Your window name");
-//        frame.setContentPane(new new_button().getJPanel());
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.pack();
-//        frame.setVisible(true);
+
+
         editor e = new editor();
         String path = "/Users/tarayounessi/Downloads/sum.py";
+        // String home = System.getProperty("user.home");
+        // File file = new File(home+"/Downloads/" + fileName + ".txt");
+
+        // ProcessBuilder pb = new ProcessBuilder("python", file).inheritIO();
         ProcessBuilder pb = new ProcessBuilder("python", path).inheritIO();
         Process p = pb.start();
         p.waitFor();
         BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line = "";
         JFrame window = new JFrame("New Window");
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
         JTextArea Text = new JTextArea(5, 25);
-        //  JButton button = new JButton();
+        JTextArea Text1 = new JTextArea(5, 25);
+        // Text.setText("HI");
+        Text1.setText("Hey");
+        p2.add(Text1);
+        // JFrame f = new JFrame("frame");
+       // JSplitPane sp = new JSplitPane(SwingConstants.VERTICAL, p1, p2);
+     //   sp.setOrientation(SwingConstants.VERTICAL);
+      //  window.add(sp);
+      //  window.setSize(300, 300);
+      //  window.setVisible(true);
+
         while ((line = bfr.readLine()) != null) {
-            Text.append(line);
+            Text.setText(line);
             // System.out.println(line);
         }
-        window.add(Text);
-        window.pack();
+        p1.add(Text);
+        //   window.add(Text);
+        //  window.pack();
+        //  window.setVisible(true);
+        JSplitPane sp = new JSplitPane(SwingConstants.VERTICAL, p1, p2);
+        sp.setOrientation(SwingConstants.VERTICAL);
+        window.add(sp);
+        window.setSize(300, 300);
         window.setVisible(true);
     }
-           
 
 }
 

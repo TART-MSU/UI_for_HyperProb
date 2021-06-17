dbbb8f4f6c343d21a4a8dea305ee4a5ef525abd3
Oyendrila Dobe <oyendrila.dobe@gmail.com>
2021-06-17 14:07:03 -0400

    Edited to show output of python in window

--- UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
+++ UI_for_hyperprob/src/UI_for_hyperprob/MainClass.java
@@ -12,38 +12,45 @@ public class MainClass {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-
         editor e = new editor();
-        String path = "/Users/tarayounessi/Downloads/sum.py";
+        String path = "sum.py";
         // String home = System.getProperty("user.home");
         // File file = new File(home+"/Downloads/" + fileName + ".txt");
 
         // ProcessBuilder pb = new ProcessBuilder("python", file).inheritIO();
-        ProcessBuilder pb = new ProcessBuilder("python", path).inheritIO();
-        Process p = pb.start();
-        p.waitFor();
+//        ProcessBuilder pb = new ProcessBuilder("python", path);
+//        pb.inheritIO();
+//        pb.redirectErrorStream(true);
+//        Process p = pb.start();
+//        p.waitFor();
+        Process p = Runtime.getRuntime().exec("python " + path);
         BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
         String line = "";
         JFrame window = new JFrame("New Window");
         JPanel p1 = new JPanel();
         JPanel p2 = new JPanel();
-        JTextArea Text = new JTextArea(5, 25);
-        JTextArea Text1 = new JTextArea(5, 25);
+        JTextArea Text = new JTextArea(5, 10);
+        JTextArea Text1 = new JTextArea(5, 10);
         // Text.setText("HI");
         Text1.setText("Hey");
         p2.add(Text1);
         // JFrame f = new JFrame("frame");
-       // JSplitPane sp = new JSplitPane(SwingConstants.VERTICAL, p1, p2);
-     //   sp.setOrientation(SwingConstants.VERTICAL);
-      //  window.add(sp);
-      //  window.setSize(300, 300);
-      //  window.setVisible(true);
+        // JSplitPane sp = new JSplitPane(SwingConstants.VERTICAL, p1, p2);
+        //   sp.setOrientation(SwingConstants.VERTICAL);
+        //  window.add(sp);
+        //  window.setSize(300, 300);
+        //  window.setVisible(true);
 
         while ((line = bfr.readLine()) != null) {
             Text.setText(line);
-            // System.out.println(line);
+            System.out.println(line);
         }
         p1.add(Text);
+        String err;
+        while ((err = stdError.readLine()) != null) {
+            System.out.println(err);
+        }
         //   window.add(Text);
         //  window.pack();
         //  window.setVisible(true);
@@ -55,4 +62,3 @@ public class MainClass {
     }
 
 }
-
---
+++ sum.py
@@ -0,0 +1,7 @@
+
+
+if __name__ == '__main__':
+    x, y = 5,6
+    sum = x+y
+    print("Sum is " + str(sum))
+
